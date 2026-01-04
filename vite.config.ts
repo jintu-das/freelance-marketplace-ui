b942cfaaa035a7fd631713efb14257f52cf40e62
Jintu Das <jintudas047@gmail.com>
2026-01-04 19:16:00 +0530

    feat: Integrate Tailwind CSS and a new UI component system (shadcn) with path aliases.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +13,11 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
     react(),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
